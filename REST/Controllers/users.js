2d522bc27920d443ae5d708e3167a7ade8ceb5f2
Vibhav Salelkar <salelkarvibhav@gmail.com>
2021-11-06 22:55:09 +0530

    feat: add POST route for CREATE  operation to create user

--- REST/Controllers/users.js
+++ REST/Controllers/users.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const users = [
     {
         firstName: "John",
@@ -13,4 +15,13 @@ const users = [
 
 export const showUsers = (req,res) => {
     res.send(users);
+}
+
+export const createUser = (req, res) => {
+    const user = req.body;
+    const userId = uuidv4();
+
+    users.push({id: userId, ...user});
+
+    res.send(`user with name ${user.firstName} added successfully`);
 }
--- REST/Routes/users.js
+++ REST/Routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {showUsers} from "../Controllers/users.js";
+import {showUsers, createUser} from "../Controllers/users.js";
 
 const router = express.Router();
 
 router.get('/', showUsers);
+router.post('/', createUser);
 
 export default router;
